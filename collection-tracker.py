20ec5d028e0c7c6171bc384059a99cf0c88a494c
reimps <109970795+cqlnx@users.noreply.github.com>
2024-02-25 20:29:28 +0200

    makes the embed have a little picture of the person who u are tracking

--- collection-tracker.py
+++ collection-tracker.py
@@ -3,12 +3,12 @@ from dhooks import Webhook, Embed
 import requests
 
 # SETTINGS CHANGE TO MATCH YOURS
-key = ""  # replace with your API key
+key = "cdd06887-8801-4a66-8815-8f8fdd858366"  # replace with your API key
 username = "popiiumaa"  # replace with your username
 profile = "Pineapple"  # replace with the profile you want to track
 collection = "SUGAR_CANE"  # replace with the collection you want
 skill = "farming"  # replace with the skill you want to track
-webhook_url = ''  # replace with your Discord webhook URL
+webhook_url = 'https://discord.com/api/webhooks/1183243149569699910/94LL-uSuRRCn2pzb-uzadWA5WhEtO_kqx0w2Eg5HS3Bfy2sVUf-4iRIrveeZEG5Cx46k'  # replace with your Discord webhook URL
 # END OF SETTINGS
 
 print(f"Your username is: {username}")
@@ -88,6 +88,8 @@ while True:
     embed.add_field(name=f"Total {skill} Skill Experience :astonished:", value=current_skill_experience, inline=False)
     embed.add_field(name=f"{skill} Skill Experience this session", value=skill_experience_session, inline=False)
     embed.add_field(name=f"Hourly Average {skill} Skill Experience", value=skill_experience_hourly_average, inline=False)
+    skin_url = f'https://minotar.net/body/{username}/300.png'
+    embed.set_thumbnail(url=skin_url)
 
     hook = Webhook(webhook_url)
     hook.send(embed=embed)
